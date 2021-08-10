4054bc2f5d7b7e77bdb7e3855744b32dbbb7275a
YIZHEN XU <ethanxu0101@gmail.com>
2021-08-10 16:09:20 +1000

    For small function testing

--- test.py
+++ test.py
@@ -1,9 +1,7 @@
 import sqlalchemy as sal
 from sqlalchemy import create_engine
-
 import sqlite3
 from sqlite3 import Error
-
 
 def create_connection(db_file):
     """ create a database connection to the SQLite database
